7a54a80bb26a2aeb1437d973afb6414c17aa986a
nishant joshi <nishantjoshi863@gmail.com>
2025-01-01 19:19:32 +0530

    changed bg video

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
     <html lang="en" className="hydrated">
       <body className="video-background">
         <video autoPlay muted loop id="video-background">
-          <source src="/bg2.mp4" type="video/mp4" />
+          <source src="/bg.mp4" type="video/mp4" />
         </video>
         <noscript>You need to enable JavaScript to run this app.</noscript>
         {children}
